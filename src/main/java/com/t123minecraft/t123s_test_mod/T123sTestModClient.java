64d43ece98830363223aea484141360e982433d4
T123Minecraft <t123minecraft@qq.com>
2026-04-26 02:04:06 +0800

    Initial commit

---
+++ src/main/java/com/t123minecraft/t123s_test_mod/T123sTestModClient.java
@@ -0,0 +1,11 @@
+package com.t123minecraft.t123s_test_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class T123sTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This code runs as soon as Minecraft is in a mod-compatible state.
+        // However, some things (like resources) may still be uninitialized.
+    }
+}
